c9c185bcbe6d117c8fb61807882b48d6904e0029
niranjan <niranjan.kvana@gmail.com>
2017-12-11 16:51:27 +0530

    port number and hostname changed to default

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', process.env.PORT || 8000);
-app.set('ip', process.env.IP || "127.0.0.1");
+var port =  process.env.PORT || 8000;
+var ip = process.env.IP || "127.0.0.1";
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
